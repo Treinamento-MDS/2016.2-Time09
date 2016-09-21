bc94ce743f5999099f74ef00386dc01dbc3da778
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-09-21 15:46:42 -0300

    ajusting models and some tests

--- saap/autenticacao/models.py
+++ saap/autenticacao/models.py
@@ -25,10 +25,6 @@ class Usuario_saap(User):
     def busca_username(cls, username):
         return Usuario_saap.objects.filter(username__startswith=username)
 
-    @classmethod
-    def get_usuario_por_username(cls, username):
-        return Usuario_saap.objects.filter(username = username)
-
 class Cidadao(Usuario_saap):
 
     pass
