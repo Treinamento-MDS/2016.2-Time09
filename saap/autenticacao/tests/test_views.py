bae0e622290d2facac402128fd71263cea5ef85a
Lucas Severo <lucassalves65@gmail.com>
2016-09-29 13:26:04 -0300

    teste de registro
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>

--- saap/autenticacao/tests/test_views.py
+++ saap/autenticacao/tests/test_views.py
@@ -2,46 +2,68 @@
 from autenticacao.views import *
 import pytest
 from django.test import Client
+from autenticacao.models import Usuario_saap, Cidadao
+
 
 @pytest.mark.django_db
 def test_login_view_post_false():
 
-	client = Client()
-	username = 'teste'
-	password = 'teste'
-	assert client.login(username=username,password=password) is False
+    client = Client()
+    username = 'teste'
+    password = 'teste'
+    assert client.login(username=username, password=password) is False
+
 
 @pytest.mark.django_db
 def test_login_view_get():
 
-	client = Client()
-	response = client.get('/login/')
-	assert response.status_code is 200
+    client = Client()
+    response = client.get('/login/')
+    assert response.status_code is 200
+
 
 def test_checar_vazio_true():
 
-	variaveis = ['teste']
-	assert checar_vazio(variaveis) 
+    variaveis = ['teste']
+    assert checar_vazio(variaveis)
+
 
 def test_checar_vazio_false():
 
-	variaveis = ['']
-	assert checar_vazio(variaveis) is False
+    variaveis = ['']
+    assert checar_vazio(variaveis) is False
+
 
 def test_checar_confirmacao():
-	return checar_confirmacao('teste','teste')
+    assert checar_confirmacao('teste', 'teste')
+
 
 def test_registro_view_get():
 
-	client = Client()
-	response = client.get('/cadastro/')
-	assert response.status_code is 200
+    client = Client()
+    response = client.get('/cadastro/')
+    assert response.status_code is 200
+
 
 @pytest.mark.django_db
-def test_registro_view_get_login():
+def test_registro_cidadao_valido():
 
-	client = Client()
-	client.login(username='test',password='test')
-	response = client.get('/cadastro/')
-	assert response.status_code is 200
+    client = Client()
+    before = len(Cidadao.objects.all())
 
+    client.post('/cadastro/', {'first_name': 'test_name',
+                               'last_name': 'test_last',
+                               'username': 'test_name',
+                               'email': 'test@email.com',
+                               'confirmacao_email': 'test@email.com',
+                               'password': '123456',
+                               'confirmacao_password': '123456',
+                               'data_de_nascimento': '1990-10-10',
+                               'sexo': 'Masculino',
+                               'municipio': 'Brasilia',
+                               'uf': 'DF'})
+
+    after = len(Cidadao.objects.all())
+
+    assert after > before
+    Cidadao.objects.get(username='test_name').delete()
