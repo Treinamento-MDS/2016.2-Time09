8f4fd5f6d5a8626deba7284be200a023fd83cc8a
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-10-25 15:50:47 -0200

    Atualizando metodo de Busca e template
    Signed-off-by: Renatafsouza <renata.rfsouza@gmail.com>
    Signed-off-by: Gustavo Sabino <gustavo.sabino@hotmail.com>

--- saap/core/views.py
+++ saap/core/views.py
@@ -317,7 +317,7 @@ class BuscaContatosView(ListView):
         if busca == 'cidade':
             resposta = Contato.objects.filter(cidade__startswith=query)
         elif busca == 'genero':
-            resposta = Contato.objects.filter(sexo__contais=query)
+            resposta = Contato.objects.filter(sexo__contains=query)
         elif busca == 'estado':
             resposta = Contato.objects.filter(estado__startswith=query)
         elif busca == 'data_aniversario':
@@ -349,3 +349,5 @@ class CriarGrupoDeContatosView(View):
         novo_grupo.save()
 
         return render(request,'contato.html')
+
+# class AdicionarContatoAoGrupo        
