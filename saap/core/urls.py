fga-eps-mds/2016.2-SAAP

Criacao da url de grupo de contatos

saap/core/urls.py
@@ -6,7 +6,7 @@ from .views import (CadastroView, DeletarContatoView, ContatoView, TicketView,
                     DeletarTicketView, GerarCartaView, CartasView,
                     DeletarCartaView, GerarPDFCartaView, EnviarCartaView,
                     BuscaContatosView, CriarGrupoDeContatosView, 
-                    AdicionarContatoAoGrupo)
+                    AdicionarContatoAoGrupo,GrupoDeContatosView)
 
 urlpatterns = [
     url(r'^cadastro_contato/$', login_required(CadastroView.as_view()),
@@ -35,10 +35,15 @@ urlpatterns = [
         name='gerar_pdf'),
     url(r'^enviar_carta/(?P<pk>[0-9]+)/$', login_required(EnviarCartaView.as_view()),
         name='enviar_carta'),
+<<<<<<< HEAD
     url(r'^criar_grupo/$', CriarGrupoDeContatosView.as_view(),
         name='criar_grupo'),
      url(r'^busca_contatos/$', BuscaContatosView.as_view(),
         name='busca_contatos'),
      url(r'^adicionar_contatos/$', AdicionarContatoAoGrupo.as_view(),
         name='adicionar_contatos'),
+=======
+    url(r'^grupo_contatos/$', GrupoDeContatosView.as_view(),
+        name='grupo_contatos'),
+>>>>>>> Criacao da url de grupo de contatos
 ]

Treinamento-MDS/2016.2-Time09

saap/core/urls.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from .views import (CadastroView, DeletarContatoView, ContatoView, TicketView, 
-                    AtualizaContato, VereadoresView, PublicarTicketView, 
-                    DeletarTicketView)
+from .views import (CadastroView, DeletarContatoView, ContatoView, TicketView,
+                    AtualizaContato, VereadoresView, PublicarTicketView,
+                    DeletarTicketView, GrupoDeContatosView)
 
 urlpatterns = [
     url(r'^cadastro_contato/$', login_required(CadastroView.as_view()),
@@ -22,4 +22,6 @@ urlpatterns = [
         name='deletar_ticket'),
     url(r'^vereadores/$', VereadoresView.as_view(),
         name='vereadores'),
+    url(r'^grupo_contatos/$', GrupoDeContatosView.as_view(),
+        name='grupo_contatos'),
 ]