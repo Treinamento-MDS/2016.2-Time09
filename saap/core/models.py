3dec50919e548b2ecae072428053a95c0b4b0761
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2016-11-05 20:12:54 -0200

    Criando model AdminGabinete

--- saap/core/models.py
+++ saap/core/models.py
@@ -105,3 +105,5 @@ class Carta(models.Model):
     forma_tratamento = models.CharField(max_length=30)
     texto = models.CharField(max_length=1500)
     data = models.DateField('data', auto_now=True)
+
+class AdminGabinete(models.Model):
